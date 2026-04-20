e13de027017950d9a971e83a8d5cd6e5a360ea3f
zlashdthestaff-code <zlashdthestaff@gmail.com>
2026-04-20 15:54:23 +0700

    Fix: Import URL facade and call configureDefaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -23,9 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    if (app()->environment('production')) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        $this->configureDefaults();
     }
 
     /**
