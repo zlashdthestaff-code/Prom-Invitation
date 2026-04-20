d0a4851c4dd142da4590c3541abc6520eb8d246c
zlashdthestaff-code <zlashdthestaff@gmail.com>
2026-04-20 16:56:37 +0700

    Final attempt: Change env to staging and force migrate

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Participant extends Model
 {
-    // Add this line below
+    use HasFactory;
+
+    // This tells Laravel exactly which table to look for
+    protected $table = 'participants';
+
+    // This allows the RSVP form to save data
     protected $fillable = ['name', 'message'];
 }
